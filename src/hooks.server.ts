c7a67882acd445141ee9887a6bf6355625bc4370
Jonas <publik.froeller.jonas@gmx.at>
2023-10-27 17:17:54 +0200

    perf(hooks): implemented html & css minifier

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,44 @@
 import { auth } from '$lib/server/lucia';
 import type { Handle } from '@sveltejs/kit';
+import { minify } from 'html-minifier';
+import { building } from '$app/environment';
+
+// https://kit.svelte.dev/docs/migrating#integrations-html-minifier
+const minification_options = {
+	useShortDoctype: true,
+	trimCustomFragments: true,
+	processConditionalComments: true,
+
+	collapseBooleanAttributes: true,
+	collapseWhitespace: true,
+	conservativeCollapse: true,
+	decodeEntities: true,
+	html5: true,
+	ignoreCustomComments: [/^#/],
+	minifyCSS: true,
+	minifyJS: false,
+	removeAttributeQuotes: true,
+	removeComments: false, // some hydration code needs comments, so leave them in
+	removeOptionalTags: true,
+	removeRedundantAttributes: true,
+	removeScriptTypeAttributes: true,
+	removeStyleLinkTypeAttributes: true,
+	sortAttributes: true,
+	sortClassName: true
+};
 
 export const handle: Handle = async ({ event, resolve }) => {
 	event.locals.auth = auth.handleRequest(event);
-	return await resolve(event);
+
+	let page = '';
+
+	return await resolve(event, {
+		transformPageChunk: ({ html, done }) => {
+			page += html;
+
+			if (done) {
+				return building ? minify(page, minification_options) : page;
+			}
+		}
+	});
 };
